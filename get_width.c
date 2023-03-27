060c7cf4f810ac888a85b96e33edcf2691577cd7
Valentine Cheluchi Agba <krixxvalle@gmail.com>
2023-03-27 18:46:44 +0000

    Hot fix

--- get_width.c
+++ get_width.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
@@ -10,7 +11,6 @@
 int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
-
 	int width = 0;
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
@@ -29,6 +29,8 @@ int get_width(const char *format, int *i, va_list list)
 		else
 			break;
 	}
+
 	*i = curr_i - 1;
+
 	return (width);
 }
