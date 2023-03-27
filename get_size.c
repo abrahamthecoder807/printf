c0f6d71f7d327d263a722b96acc02d04bf837360
Valentine Cheluchi Agba <krixxvalle@gmail.com>
2023-03-27 14:40:59 +0000

    A printf project collaboration by Abraham and Valentine

--- get_size.c
+++ get_size.c
@@ -10,7 +10,7 @@
 int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
-	int size = o;
+	int size = 0;
 
 	if (format[curr_i] == 'I')
 		size = S_LONG;
--- main.h
+++ main.h
@@ -104,7 +104,7 @@ char buffer[],
 /****************** UTILS ******************/
 
 int is_printable(char);
-int append_hexa_code(char, char[], int);
+int append_hexa_code(unsigned char, char[], int);
 int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
